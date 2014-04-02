22b89f785b489d4b8837f2342ff57fb7aad7e43b
Yue Ma <yuem@codeaurora.org>
2014-04-02 16:50:41 -0700

    wcnss: Send FIQ to firmware after smp2p error ready happens
    In order to get firmware dump, send FIQ when it times out on smp2p
    error ready during cold boot.
    
    Change-Id: I5edfed83dc12b00865066f2c8028d34b9857e116
    CRs-Fixed: 608693
    Signed-off-by: Yue Ma <yuem@codeaurora.org>
    
    Conflicts:
    	drivers/net/wireless/wcnss/wcnss_wlan.c

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -2607,6 +2607,7 @@ wcnss_trigger_config(struct platform_device *pdev)
 	} while (pil_retry++ < WCNSS_MAX_PIL_RETRY && IS_ERR(penv->pil));
 
 	if (pil_retry >= WCNSS_MAX_PIL_RETRY) {
+		wcnss_reset_intr();
 		if (penv->wcnss_notif_hdle)
 			subsys_notif_unregister_notifier(penv->wcnss_notif_hdle,
 				&wnb);
